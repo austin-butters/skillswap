0d6b4e549a5ea39556da832a116e518670a6be3e
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 08:50:00 +1200

    Added basic inbox component

---
+++ client/components/Inbox.tsx
@@ -0,0 +1,58 @@
+import { Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
+
+export default function Inbox() {
+  const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  return (
+    <>
+      <Sidebar />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '75vw',
+          backgroundColor: 'gray',
+          borderRadius: '20px',
+          alignItems: 'center',
+          padding: '20px',
+          height: '75vh',
+          marginTop: '12.5vh',
+          overflow: 'scroll',
+          scrollbarWidth: 'none',
+        }}
+      >
+        <h1
+          style={{
+            fontWeight: 'bold',
+            fontSize: '2.5rem',
+            marginBottom: '20px',
+          }}
+        >
+          Inbox
+        </h1>
+        {testArr.map((num) => {
+          //Doing this so there isnt a bunch of html clogging the file so I can test scrolling
+          return (
+            <Link
+              key={num}
+              to={`/message/2`}
+              style={{
+                backgroundColor: 'lightgray',
+                display: 'flex',
+                width: '80%',
+                padding: '10px 20px',
+                justifyContent: 'space-between',
+                borderRadius: '10px',
+                alignItems: 'center',
+                marginBottom: '10px',
+              }}
+            >
+              <h1 style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>User 2</h1>
+              <p>This is the most recent message...</p>
+            </Link>
+          )
+        })}
+      </div>
+    </>
+  )
+}
