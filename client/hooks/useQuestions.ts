51d47b5b4ae41809a33a664e39b759669e58177d
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 11:13:43 +1200

    Created a hook function to add a question

---
+++ client/hooks/useQuestions.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import request from 'superagent'
+
+export function useAddQuestion() {
+  const queryClient = useQueryClient()
+  useMutation({
+    mutationFn: async (question: {
+      userId: number
+      title: string
+      body: string
+    }) => {
+      const response = await request.post(`/api/v1/questions`).send(question)
+      return response.body
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['questions'] }),
+  })
+}
