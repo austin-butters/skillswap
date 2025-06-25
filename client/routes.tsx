859184d8add03693187195c159956d7dcd5f868a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 08:51:51 +1200

    Adding routing for all the new components

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,19 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import MessageBox from './components/MessageBox.tsx'
+import Connect from './components/Connect.tsx'
+import CodeFixer from './components/CodeFixer.tsx'
+import Profile from './components/Profile.tsx'
+import Inbox from './components/Inbox.tsx'
+import Call from './components/Call.tsx'
+export default createRoutesFromElements(
+  <>
+    <Route index element={<App />} />
+    <Route path="/message/:id" element={<MessageBox />} />
+    <Route path="/connect" element={<Connect />} />
+    <Route path="/codeFixer" element={<CodeFixer />} />
+    <Route path="/profile/:id" element={<Profile />} />
+    <Route path="/inbox/:id" element={<Inbox />} />
+    <Route path="/call/:id" element={<Call />} />
+  </>,
+)
