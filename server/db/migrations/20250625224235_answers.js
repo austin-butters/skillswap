e50a44de724a2c9dfbab73b9ef99c4cbdf80da95
Luke <59680394+luke-mai-devacd@users.noreply.github.com>
2025-06-26 10:55:32 +1200

    Add answers migration

---
+++ server/db/migrations/20250625224235_answers.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('answers', (table) => {
+    table.increments('id').primary()
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+    table
+      .integer('question_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('questions')
+    table.integer('reply_to').defaultTo(null)
+    table.text('body').notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('answers')
+}
