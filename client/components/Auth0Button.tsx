7356ecd7b13a59857191b3eae24cb7240e8e1c01
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 09:45:34 +1200

    Added handleLogout function

--- client/components/Auth0Button.tsx
+++ client/components/Auth0Button.tsx
@@ -1,10 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
 export default function Auth0Button() {
-  const { loginWithRedirect } = useAuth0()
+  const { loginWithRedirect, logout } = useAuth0()
 
   function handleLogin() {
     loginWithRedirect()
+  }
+
+  function handleLogout() {
+    logout()
   }
 
   return <button onClick={handleLogin}>Log In</button>
