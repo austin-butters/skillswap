bd06a5a08c6de7151f7e45d99b4bcdfa10b0a85a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 11:41:25 +1200

    Removed fruit stuff because it was causing server to crash

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,12 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-import fruitRoutes from './routes/fruits.ts'
 import questionRoutes from './routes/questions.ts'
 
 const server = express()
 
 server.use(express.json())
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/questions', questionRoutes)
 
 if (process.env.NODE_ENV === 'production') {
