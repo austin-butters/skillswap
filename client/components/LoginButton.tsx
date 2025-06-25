d1d014e1ceeebca16d58027bfb72309f35f8efda
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 09:42:45 +1200

    Added handle login function

--- client/components/LoginButton.tsx
+++ client/components/LoginButton.tsx
@@ -2,5 +2,10 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0()
+
+  function handleLogin() {
+    loginWithRedirect()
+  }
+
   return <button>Log In</button>
 }
