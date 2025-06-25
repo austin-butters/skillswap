13c7d097124c4408611df6d503e211f8e82c8295
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 09:44:46 +1200

    Changed login component name to Auth0Button due to wanting to wanting to return a logout button if already logged in

--- client/components/Auth0Button.tsx
+++ client/components/Auth0Button.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
-export default function LoginButton() {
+export default function Auth0Button() {
   const { loginWithRedirect } = useAuth0()
 
   function handleLogin() {
