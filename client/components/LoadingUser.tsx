eed279efae496a1e8c18a6267d3c3309317231c6
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-26 12:52:51 +1200

    Added some checks which will lead to adding a user

--- client/components/LoadingUser.tsx
+++ client/components/LoadingUser.tsx
@@ -1,8 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useAuth0Id } from '../hooks/useUsers'
+import { useEffect, useState } from 'react'
 
 export default function LoadingAccount() {
-  const { user: auth0User } = useAuth0()
+  const { user: auth0User, isAuthenticated, isLoading } = useAuth0()
+
+  const [userAdded, setUserAdded] = useState(false)
 
   //Here you would check the database for the user using the auth0 sub i think?
   //If there isnt then post info to database using the user object
@@ -12,6 +15,24 @@ export default function LoadingAccount() {
   //I think you'll have to import navigate from react-router-dom by the way
 
   const { data: userData, isLoading: userLoading } = useAuth0Id(auth0User?.sub)
+
+  if (userData) {
+    setUserAdded(true)
+  }
+
+  useEffect(() => {
+    if (
+      isAuthenticated &&
+      auth0User &&
+      !isLoading &&
+      !userData &&
+      !userLoading &&
+      !userAdded
+    ) {
+      console.log('Adding user')
+      setUserAdded(true)
+    }
+  }, [auth0User, isAuthenticated, isLoading, userAdded, userData, userLoading])
 
   return (
     <div
@@ -27,9 +48,9 @@ export default function LoadingAccount() {
       <span className="loading-indicator" />
       {!auth0User ? (
         <h1>Connecting</h1>
-      ) : !userLoading ? (
+      ) : userLoading ? (
         <h1>Grabbing user info</h1>
-      ) : !userData ? (
+      ) : !userAdded ? (
         <h1>Creating account</h1>
       ) : (
         <h1>Done!</h1>
