acb15f3a5366d90dac53a8c859d258137805cb7c
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-27 09:49:51 +1200

    Added type to userInput

--- client/api/ai.ts
+++ client/api/ai.ts
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 const rootURL = new URL('/api/v1', document.baseURI)
 
-export async function getAiResponse(userInput) {
+export async function getAiResponse(userInput: string) {
   const response = await request.get(`${rootURL}/ai?prompt=${userInput}`)
   return `${response.body[0].content.parts[0].text}`
 }
