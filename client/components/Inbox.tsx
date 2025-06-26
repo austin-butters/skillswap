3523cd5bfaae2baa55e075902a4d3eec6a575b82
Marcelo Reyes-Tokuma <reyesTbiz@gmail.com>
2025-06-27 10:39:54 +1200

    pulling down

--- client/components/Inbox.tsx
+++ client/components/Inbox.tsx
@@ -6,50 +6,23 @@ export default function Inbox() {
   return (
     <>
       <Sidebar />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '75vw',
-          backgroundColor: 'gray',
-          borderRadius: '20px',
-          alignItems: 'center',
-          padding: '20px',
-          height: '75vh',
-          marginTop: '12.5vh',
-          overflow: 'scroll',
-          scrollbarWidth: 'none',
-        }}
-      >
-        <h1
-          style={{
-            fontWeight: 'bold',
-            fontSize: '2.5rem',
-            marginBottom: '20px',
-          }}
-        >
-          Inbox
-        </h1>
+      <div className="header">
+        <h1>Inbox</h1>
+      </div>
+      <div className="inbox-box">
         {testArr.map((num) => {
           //Doing this so there isnt a bunch of html clogging the file so I can test scrolling
           return (
-            <Link
-              key={num}
-              to={`/message/2`}
-              style={{
-                backgroundColor: 'lightgray',
-                display: 'flex',
-                width: '80%',
-                padding: '10px 20px',
-                justifyContent: 'space-between',
-                borderRadius: '10px',
-                alignItems: 'center',
-                marginBottom: '10px',
-              }}
-            >
-              <h1 style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>User 2</h1>
-              <p>This is the most recent message...</p>
-            </Link>
+            <>
+              <div className="inbox-message">
+                <Link key={num} to={`/message/2`}>
+                  <h1 style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+                    User 2
+                  </h1>
+                  <p>This is the most recent message...</p>
+                </Link>
+              </div>
+            </>
           )
         })}
       </div>
