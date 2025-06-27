1aa0a5101e29c1436240537ed23a03abfc9d2405
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-28 11:21:10 +1200

    Hook function sending a direct message

--- client/hooks/useDirectMessages.ts
+++ client/hooks/useDirectMessages.ts
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query'
-import { getDirectMessages } from 'client/api/directMessages'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getDirectMessages, sendDirectMessage } from 'client/api/directMessages'
 
 export function useGetDirectMessages(userId: number, otherId: number) {
   const query = useQuery({
@@ -7,4 +7,26 @@ export function useGetDirectMessages(userId: number, otherId: number) {
     queryFn: () => getDirectMessages(userId, otherId),
   })
   return query
+}
+
+export function useSendDirectMessage() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ({
+      userId,
+      receiverId,
+      time,
+      body,
+    }: {
+      userId: number
+      receiverId: number
+      time: string
+      body: string
+    }) => sendDirectMessage(userId, receiverId, time, body),
+    onSuccess: (data, { userId, receiverId }) => {
+      queryClient.invalidateQueries({
+        queryKey: ['directMessages', userId, receiverId],
+      })
+    },
+  })
 }
