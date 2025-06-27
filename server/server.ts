c93f7e6f2a0419a746b28dba4597ed6c9c5c8c2d
Austin <198062900+austin-butters@users.noreply.github.com>
2025-06-27 15:59:39 +1200

    Add server side functionality for answers

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import * as Path from 'node:path'
 import userRoutes from './routes/users'
 import questionRoutes from './routes/questions.ts'
 import aiRoutes from './routes/ai.ts'
+import answerRoutes from './routes/answers.ts'
 
 const server = express()
 
@@ -11,6 +12,7 @@ server.use(express.json())
 server.use('/api/v1/users', userRoutes)
 server.use('/api/v1/questions', questionRoutes)
 server.use('/api/v1/ai', aiRoutes)
+server.use('/api/v1/answers', answerRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
