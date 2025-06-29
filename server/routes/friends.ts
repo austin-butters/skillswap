60f59674e89534b032de8a2771d258bce23dfca7
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 10:49:23 +1200

    Server route for unadding a user

--- server/routes/friends.ts
+++ server/routes/friends.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
-import { addFriend, checkStatus } from 'server/db/db-functions/friends'
+import {
+  addFriend,
+  checkStatus,
+  unaddFriend,
+} from 'server/db/db-functions/friends'
 
 const router = Router()
 
@@ -8,6 +12,18 @@ router.post('/:userId/:requestId', async (req, res) => {
   const requestId = Number(req.params.requestId)
   try {
     const response = await addFriend(userId, requestId)
+    return res.status(200).json(response)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.delete('/:userId/:requestId', async (req, res) => {
+  const userId = Number(req.params.userId)
+  const requestId = Number(req.params.requestId)
+  try {
+    const response = await unaddFriend(userId, requestId)
     return res.status(200).json(response)
   } catch (err) {
     console.log(err)
