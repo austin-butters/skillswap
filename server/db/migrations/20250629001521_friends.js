fa577378bacb3506a5b873910cde98184d663c89
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-29 12:17:55 +1200

    Added a table for friends

---
+++ server/db/migrations/20250629001521_friends.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+    table
+      .integer('user_friended_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('users')
+}
