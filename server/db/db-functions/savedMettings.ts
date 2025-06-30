17c6e99fa9581f7cda071c6d91b8a7284c307af1
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 15:53:20 +1200

    Database function to get all saved meetings from a user

--- server/db/db-functions/savedMettings.ts
+++ server/db/db-functions/savedMettings.ts
@@ -3,3 +3,7 @@ import db from '../connection'
 export async function saveMeeting(userId: number, meetingId: number) {
   await db('saved_meetings').insert({ user_id: userId, meeting_id: meetingId })
 }
+
+export async function getSavedMeetings(userId: number) {
+  return await db('saved_meetings').where({ user_id: userId }).select()
+}
