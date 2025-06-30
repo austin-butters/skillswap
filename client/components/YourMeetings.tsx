9a440d30fa0f2c62aeeb74802a20889b1a484d87
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 15:28:07 +1200

    Added a component to list off a users meetings

--- client/components/YourMeetings.tsx
+++ client/components/YourMeetings.tsx
@@ -1,10 +1,37 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { useGetUsersMeetings } from 'client/hooks/useMeetings'
 import { useAuth0Id } from 'client/hooks/useUsers'
+import { Link } from 'react-router-dom'
+import { meetingData } from 'server/db/db-functions/meetings'
 
 export default function YourMeetings() {
   const { user } = useAuth0()
 
   const { data: userData } = useAuth0Id(user?.sub)
 
-  return <p>Testing...</p>
+  const { data: meetingsData } = useGetUsersMeetings(userData?.id)
+
+  if (!meetingsData || !userData) {
+    return <p>Loading meetings...</p>
+  }
+
+  return (
+    <>
+      <h1>{userData.name}s meetings</h1>
+      <ul>
+        {meetingsData.map((meeting: meetingData, i: number) => {
+          return (
+            <li key={`Meeting ${i}`}>
+              <a href={meeting.url} target="_blank" rel="noreferrer">
+                {meeting.title}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/meeting/create">
+        <button>Create a meeting!</button>
+      </Link>
+    </>
+  )
 }
