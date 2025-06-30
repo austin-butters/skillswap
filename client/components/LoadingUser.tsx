f464b64b50e386c5d81579908284f6a0272852b9
Austin <198062900+austin-butters@users.noreply.github.com>
2025-06-30 12:24:12 +1200

    Fix unauthorized error on registration

--- client/components/LoadingUser.tsx
+++ client/components/LoadingUser.tsx
@@ -20,7 +20,8 @@ export default function LoadingAccount() {
       !userLoading &&
       !userAdded
     ) {
-      addUser.mutate(
+      console.log('USEEFFECT') // TEST LOG
+      addUser.mutateAsync(
         {
           auth0Uid: String(auth0User.sub),
           email: String(auth0User.email),
@@ -39,13 +40,14 @@ export default function LoadingAccount() {
         },
       )
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth0User, isAuthenticated, isLoading, userAdded, userData, userLoading])
 
   useEffect(() => {
     if (userAdded || userData) {
-      navigate(`/profile/${userData.id}`)
+      navigate(userData?.id ? `/profile/${userData.id}` : `/`)
     }
-  }, [userAdded, userData])
+  }, [userAdded, userData, navigate])
 
   return (
     <div
