6b08d6a8a6c5ef2e22335b1746a4a8d8328eb426
Luke <59680394+luke-mai-devacd@users.noreply.github.com>
2025-07-01 11:29:58 +1200

    Bio-class

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,10 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useAuth0Id } from 'client/hooks/useUsers'
+import { useParams } from 'react-router-dom'
 
 export default function Profile() {
   const { user } = useAuth0()
   const { data: userData, isLoading, isError } = useAuth0Id(user?.sub)
-
+  const id = useParams().id
   if (isLoading) {
     return <p>Loading...</p>
   }
@@ -26,6 +27,7 @@ export default function Profile() {
           <div className="user-section">
             <div className="user-greeting">
               <h1>{`Hi ${userData.name}!`}</h1>
+              <p className="user-bio">{userData.bio}</p>
             </div>
 
             <div className="user-details-box">
