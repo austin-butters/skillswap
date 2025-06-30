5f8b81bc925ab9080ce2cac74063539038725a02
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-01 10:50:02 +1200

    Api functions for meetings now send a token

--- client/api/meetings.ts
+++ client/api/meetings.ts
@@ -1,24 +1,41 @@
+import { JWT, MeetingData } from '#models'
 import { meetingData } from '../../server/db/db-functions/meetings'
 import request from 'superagent'
 
 const rootURL = new URL('/api/v1', document.baseURI)
 
-export async function createMeeting(meetingData: meetingData) {
-  const response = await request.post(`${rootURL}/meetings`).send(meetingData)
+export async function createMeeting(
+  meetingData: meetingData,
+  token: JWT,
+): Promise<void> {
+  const response = await request
+    .post(`${rootURL}/meetings`)
+    .set('Authorization', `Bearer ${token}`)
+    .send(meetingData)
   return response.body
 }
 
-export async function getUserMeetings(userId: number) {
-  const response = await request.get(`${rootURL}/meetings/user/${userId}`)
+export async function getUserMeetings(
+  userId: number,
+  token: JWT,
+): Promise<MeetingData[]> {
+  const response = await request
+    .get(`${rootURL}/meetings/user/${userId}`)
+    .set('Authorization', `Bearer ${token}`)
   return response.body
 }
 
-export async function getPublicMeetings() {
+export async function getPublicMeetings(): Promise<MeetingData[]> {
   const response = await request.get(`${rootURL}/meetings/public/all`)
   return response.body
 }
 
-export async function getMeetingById(meetingId: number) {
-  const response = await request.get(`${rootURL}/meetings/meeting/${meetingId}`)
+export async function getMeetingById(
+  meetingId: number,
+  token: JWT,
+): Promise<MeetingData> {
+  const response = await request
+    .get(`${rootURL}/meetings/meeting/${meetingId}`)
+    .set('Authorization', `Bearer ${token}`)
   return response.body
 }
