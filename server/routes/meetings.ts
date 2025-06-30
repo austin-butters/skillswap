ac24fb08287ceb43f61391334f9c4cf4fb2af92a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 14:50:52 +1200

    Fixed how meeting data was being grabbed

--- server/routes/meetings.ts
+++ server/routes/meetings.ts
@@ -4,7 +4,7 @@ import { addMeeting } from 'server/db/db-functions/meetings'
 const router = Router()
 
 router.post('/', async (req, res) => {
-  const meetingData = req.query.data
+  const meetingData = req.body
   try {
     const response = await addMeeting(meetingData)
     return res.json(200).json(response)
