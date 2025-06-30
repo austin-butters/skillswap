bb516f68700b33a1ea8118222c01077a3707bdbe
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 14:52:52 +1200

    Server route for grabbing a users meetings

--- server/routes/meetings.ts
+++ server/routes/meetings.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addMeeting } from 'server/db/db-functions/meetings'
+import { addMeeting, getUsersMeetings } from 'server/db/db-functions/meetings'
 
 const router = Router()
 
@@ -7,7 +7,18 @@ router.post('/', async (req, res) => {
   const meetingData = req.body
   try {
     const response = await addMeeting(meetingData)
-    return res.json(200).json(response)
+    return res.status(200).json(response)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const userId = Number(req.params.id)
+  try {
+    const response = await getUsersMeetings(userId)
+    return res.status(200).json(response)
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: 'Something went wrong' })
