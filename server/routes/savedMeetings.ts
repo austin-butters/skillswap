05c8d76605adefab4245869f8fe2792b0f3b3623
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 15:56:19 +1200

    Server route getting saved routes for a certain user

--- server/routes/savedMeetings.ts
+++ server/routes/savedMeetings.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { saveMeeting } from '../db/db-functions/savedMettings'
+import { getSavedMeetings, saveMeeting } from '../db/db-functions/savedMettings'
 
 const router = Router()
 
@@ -8,6 +8,17 @@ router.post('/:userId/:meetingId', async (req, res) => {
   const meetingId = Number(req.params.meetingId)
   try {
     const response = await saveMeeting(userId, meetingId)
+    return res.status(200).json(response)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:userId', async (req, res) => {
+  const userId = Number(req.params.userId)
+  try {
+    const response = await getSavedMeetings(userId)
     return res.status(200).json(response)
   } catch (err) {
     console.log(err)
