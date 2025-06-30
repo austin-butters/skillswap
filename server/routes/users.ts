a815621b1e3248ed53ab56ca242fb72ae23b0689
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-01 09:20:59 +1200

    Removed check JWT from post route for users

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -3,30 +3,19 @@
 //====================================================================================================
 
 // ------------------------------ SETUP ------------------------------ //
-import { Auth0Uid, UnassignedUser, User, UserEmail, UserId } from '#models'
+import { UnassignedUser, User, UserEmail, UserId } from '#models'
 import express from 'express'
 import { Users } from '../db/db-functions'
 
 import checkJwt from '../auth0'
-import type { JwtRequest } from '../auth0'
 
 const router = express.Router()
 
 // ------------------------------ CREATE ------------------------------ //
-router.post('/', checkJwt, async (req: JwtRequest, res) => {
+router.post('/', async (req, res) => {
   console.log('server route: users, POST /') // TEST LOG
   try {
-    const auth0UidFromReq: Auth0Uid | undefined = req.auth?.sub
-
-    if (!auth0UidFromReq) {
-      return res.status(401).json({ error: 'Unauthorized' })
-    }
-
     const { auth0Uid, email, name, bio, profilePicture } = req.body
-
-    if (auth0UidFromReq !== auth0Uid) {
-      return res.status(401).json({ error: 'Unauthorized' })
-    }
 
     if (
       typeof auth0Uid !== 'string' ||
