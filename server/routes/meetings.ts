9194d2be5ca624ddc2093b9eba02a78519da2d5c
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 16:09:40 +1200

    Server route to get all public meetings

--- server/routes/meetings.ts
+++ server/routes/meetings.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
-import { addMeeting, getUsersMeetings } from '../db/db-functions/meetings'
+import {
+  addMeeting,
+  getPublicMeetings,
+  getUsersMeetings,
+} from '../db/db-functions/meetings'
 
 const router = Router()
 
@@ -18,6 +22,16 @@ router.get('/:id', async (req, res) => {
   const userId = Number(req.params.id)
   try {
     const response = await getUsersMeetings(userId)
+    return res.status(200).json(response)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/public', async (req, res) => {
+  try {
+    const response = await getPublicMeetings()
     return res.status(200).json(response)
   } catch (err) {
     console.log(err)
