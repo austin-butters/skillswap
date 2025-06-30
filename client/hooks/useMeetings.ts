02e1b19ba89f1787316024fa199bd50e841fc1dd
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-30 15:26:41 +1200

    Hook function to get all meetings from a user

--- client/hooks/useMeetings.ts
+++ client/hooks/useMeetings.ts
@@ -1,9 +1,23 @@
-import { useMutation } from '@tanstack/react-query'
-import { createMeeting } from 'client/api/meetings'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { createMeeting, getUserMeetings } from 'client/api/meetings'
 import { meetingData } from 'server/db/db-functions/meetings'
 
 export function useCreateMeeting() {
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: (meetingData: meetingData) => createMeeting(meetingData),
+    onSuccess: (data, params) => {
+      queryClient.invalidateQueries({
+        queryKey: ['meetings', params.hostId],
+      })
+    },
   })
+}
+
+export function useGetUsersMeetings(userId: number) {
+  const query = useQuery({
+    queryKey: ['meetings', userId],
+    queryFn: () => getUserMeetings(userId),
+  })
+  return query
 }
