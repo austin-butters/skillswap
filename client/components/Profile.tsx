1f62c26d988390a57e2792b7be65ebac81786a0c
Luke <59680394+luke-mai-devacd@users.noreply.github.com>
2025-07-01 10:42:12 +1200

    Profile-fix

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -3,50 +3,62 @@ import { useAuth0Id } from 'client/hooks/useUsers'
 
 export default function Profile() {
   const { user } = useAuth0()
-  const { data: userData } = useAuth0Id(user?.sub)
+  const { data: userData, isLoading, isError } = useAuth0Id(user?.sub)
 
-  return (
-    <>
-      <div className="user-dashboard">
-        {/* User Info Section */}
-        <div className="user-section">
-          <div className="user-greeting">
-            <h1>{`Hi ${userData.name}!`}</h1>
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (!userData) {
+    return (
+      <>
+        <p>No user found, are you logged in?</p>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className="user-dashboard">
+          {/* User Info Section */}
+          <div className="user-section">
+            <div className="user-greeting">
+              <h1>{`Hi ${userData.name}!`}</h1>
+            </div>
+
+            <div className="user-details-box">
+              <h1>User details:</h1>
+              <h2>
+                Name: <input type="text" />
+              </h2>
+              <h2>
+                Bio: <input type="text" />
+              </h2>
+              <button className="update-button">Update</button>
+            </div>
           </div>
 
-          <div className="user-details-box">
-            <h1>User details:</h1>
-            <h2>
-              Name: <input type="text" />
-            </h2>
-            <h2>
-              Bio: <input type="text" />
-            </h2>
-            <button className="update-button">Update</button>
+          {/* Past Solutions Section */}
+          <div className="solutions-section">
+            <h1>Past solutions</h1>
+
+            <div className="solution-card">
+              <h1>Title</h1>
+              <p>
+                Description blah blah blah blah blah blah blah blah blah blah
+                blah blah blah blah blah blah
+              </p>
+            </div>
+
+            <div className="solution-card">
+              <h1>Title</h1>
+              <p>
+                Description blah blah blah blah blah blah blah blah blah blah
+                blah blah blah blah blah blah
+              </p>
+            </div>
           </div>
         </div>
-
-        {/* Past Solutions Section */}
-        <div className="solutions-section">
-          <h1>Past solutions</h1>
-
-          <div className="solution-card">
-            <h1>Title</h1>
-            <p>
-              Description blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah
-            </p>
-          </div>
-
-          <div className="solution-card">
-            <h1>Title</h1>
-            <p>
-              Description blah blah blah blah blah blah blah blah blah blah blah
-              blah blah blah blah blah
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
