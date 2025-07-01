75386fe4097819a3c921b42a73751ca5d3735871
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-02 09:38:17 +1200

    Database function to remove saved meeting

--- server/db/db-functions/savedMettings.ts
+++ server/db/db-functions/savedMettings.ts
@@ -1,4 +1,4 @@
-import { SavedMeeting } from '#models'
+import { SavedMeeting, UserId } from '#models'
 import db from '../connection'
 
 export async function saveMeeting(
@@ -21,4 +21,10 @@ export async function getSavedMeetings(
   userId: number,
 ): Promise<SavedMeeting[]> {
   return await db('saved_meetings').where({ user_id: userId }).select()
+}
+
+export async function removeSavedMeeting(userId: UserId, meetingId: number) {
+  await db('saved_meetings')
+    .where({ user_id: userId, meeting_id: meetingId })
+    .del()
 }
