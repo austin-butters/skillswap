b700c0dd413938e5db5b2c341ab191eecd275a35
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-02 10:08:34 +1200

    Meeting page includes checks to let user know if they dont have any meetings or any saved meetings

--- client/components/YourMeetings.tsx
+++ client/components/YourMeetings.tsx
@@ -33,60 +33,72 @@ export default function YourMeetings() {
       <div>
         <h1>{userData.name}s meetings</h1>
         <ul>
-          {meetingsData.map((meeting: MeetingData, i: number) => {
-            return (
-              <li key={`Meeting ${i}`}>
-                <a href={meeting.url} target="_blank" rel="noreferrer">
-                  {meeting.title}
-                </a>
-              </li>
-            )
-          })}
+          {meetingsData.length === 0 ? (
+            <p>No meetings...</p>
+          ) : (
+            meetingsData.map((meeting: MeetingData, i: number) => {
+              return (
+                <li key={`Meeting ${i}`}>
+                  <a href={meeting.url} target="_blank" rel="noreferrer">
+                    {meeting.title}
+                  </a>
+                </li>
+              )
+            })
+          )}
         </ul>
         <Link to="/meeting/create">
           <button>Create a meeting!</button>
         </Link>
       </div>
       <div>
-        <h1>Reccomended meetings</h1>
+        <h1>Reccomended public meetings</h1>
         <ul>
-          {publicMeetings.map((meeting, i) => {
-            if (meeting.host_id === userData.id) {
-              return null
-            }
-            if (
-              savedMeetings.some(
-                (savedMeeting: SavedMeetingData) =>
-                  meeting.id === savedMeeting.meeting_id,
+          {publicMeetings.length === 0 ? (
+            <p>No meetings...</p>
+          ) : (
+            publicMeetings.map((meeting, i) => {
+              if (meeting.host_id === userData.id) {
+                return null
+              }
+              if (
+                savedMeetings.some(
+                  (savedMeeting: SavedMeetingData) =>
+                    meeting.id === savedMeeting.meeting_id,
+                )
+              ) {
+                return null
+              }
+              return (
+                <li key={`Public meeting ${i}`}>
+                  <a href={meeting.url} target="_blank" rel="noreferrer">
+                    {meeting.title}
+                  </a>
+                  <button onClick={() => handleSave(meeting.id)}>
+                    Save Meeting
+                  </button>
+                </li>
               )
-            ) {
-              return null
-            }
-            return (
-              <li key={`Public meeting ${i}`}>
-                <a href={meeting.url} target="_blank" rel="noreferrer">
-                  {meeting.title}
-                </a>
-                <button onClick={() => handleSave(meeting.id)}>
-                  Save Meeting
-                </button>
-              </li>
-            )
-          })}
+            })
+          )}
         </ul>
       </div>
       <div>
         <h1>Saved meetings</h1>
-        {savedMeetings.map((meeting: SavedMeetingData, i: number) => {
-          return (
-            <li key={`Saved meeting ${i}`}>
-              <SavedMeeting
-                meetingId={Number(meeting.meeting_id)}
-                userId={Number(userData.id)}
-              />
-            </li>
-          )
-        })}
+        {savedMeetings.length === 0 ? (
+          <p>No saved meetings...</p>
+        ) : (
+          savedMeetings.map((meeting: SavedMeetingData, i: number) => {
+            return (
+              <li key={`Saved meeting ${i}`}>
+                <SavedMeeting
+                  meetingId={Number(meeting.meeting_id)}
+                  userId={Number(userData.id)}
+                />
+              </li>
+            )
+          })
+        )}
       </div>
     </div>
   )
