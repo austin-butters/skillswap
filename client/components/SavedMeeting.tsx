40f4cb9267400c19fe51f058b4422f7674c1b10c
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-02 09:53:26 +1200

    Updated saved meeting component to also include a button to remove the meeting from saved list

--- client/components/SavedMeeting.tsx
+++ client/components/SavedMeeting.tsx
@@ -1,7 +1,25 @@
+import { useRemoveSavedMeeting } from 'client/hooks/useSavedMeetings'
 import { useGetMeetingById } from '../hooks/useMeetings'
+import { UserId } from '#models'
 
-export default function SavedMeeting({ meetingId }: { meetingId: number }) {
+export default function SavedMeeting({
+  meetingId,
+  userId,
+}: {
+  meetingId: number
+  userId: UserId
+}) {
   const { data: meetingData } = useGetMeetingById(meetingId)
+
+  const removeSavedMeeting = useRemoveSavedMeeting()
+
+  function handleRemoveSave(meetingId: number) {
+    console.log(`Removing meeting ${meetingId}, for user ${userId}`)
+    removeSavedMeeting.mutate({
+      userId: userId,
+      meetingId: meetingId,
+    })
+  }
 
   if (!meetingData) {
     return <p>Loading..</p>
@@ -12,6 +30,9 @@ export default function SavedMeeting({ meetingId }: { meetingId: number }) {
       <a href={meetingData.url} target="_blank" rel="noreferrer">
         {meetingData.title}
       </a>
+      <button onClick={() => handleRemoveSave(meetingData.id)}>
+        Unsave meeting
+      </button>
     </>
   )
 }
