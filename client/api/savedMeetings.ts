44f69cc7eefbef40214a732593945a515c89958f
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-02 09:43:46 +1200

    Api function to remove a saved meeting

--- client/api/savedMeetings.ts
+++ client/api/savedMeetings.ts
@@ -1,4 +1,4 @@
-import { JWT } from '#models'
+import { JWT, UserId } from '#models'
 import request from 'superagent'
 
 const rootURL = new URL('/api/v1', document.baseURI)
@@ -17,6 +17,17 @@ export async function saveMeeting(
 export async function getSavedMeetings(userId: number, token: JWT) {
   const response = await request
     .get(`${rootURL}/savedMeetings/${userId}`)
+    .set('Authorization', `Bearer ${token}`)
+  return response.body
+}
+
+export async function removeSavedMeeting(
+  userId: UserId,
+  meetingId: number,
+  token: JWT,
+) {
+  const response = await request
+    .del(`${rootURL}/savedMeetings/${userId}/${meetingId}`)
     .set('Authorization', `Bearer ${token}`)
   return response.body
 }
