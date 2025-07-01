88ea3262722ad9dcb7859149bf1f2ba50357a154
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-02 11:00:30 +1200

    If a user is not logged in sidebar only shows home button

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -2,11 +2,23 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Link } from 'react-router-dom'
 import '../styles/main.css'
 import Auth0Button from './Auth0Button'
+import { useAuth0Id } from 'client/hooks/useUsers'
 export default function Sidebar() {
-  const { isAuthenticated } = useAuth0()
+  const { isAuthenticated, user } = useAuth0()
   const loginPageLinkPath: string = '/login'
 
+  const { data: userData } = useAuth0Id(user?.sub)
+
+  if (!userData && isAuthenticated) {
+    return (
+      <div>
+        <div className="sidebar"></div>
+      </div>
+    )
+  }
+
   const userId = 1
+
   return (
     <div>
       <div className="sidebar">
@@ -18,41 +30,49 @@ export default function Sidebar() {
               <Link to="/">Home</Link>
             </div>
           </li>
-          <li>
-            <div className="sidebar-button">
-              <Link to={isAuthenticated ? '/connect' : loginPageLinkPath}>
-                Connect
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div className="sidebar-button">
-              <Link to={isAuthenticated ? '/codeFixer' : loginPageLinkPath}>
-                CodeFixer
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div className="sidebar-button">
-              <Link to={isAuthenticated ? `/inbox` : loginPageLinkPath}>
-                Inbox
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div className="sidebar-button">
-              <Link
-                to={isAuthenticated ? `/profile/${userId}` : loginPageLinkPath}
-              >
-                Profile
-              </Link>
-            </div>
-            <div className="sidebar-button">
-              <Link to={isAuthenticated ? '/yourMeetings' : loginPageLinkPath}>
-                Meetings
-              </Link>
-            </div>
-          </li>
+          {!isAuthenticated ? null : (
+            <>
+              <li>
+                <div className="sidebar-button">
+                  <Link to={isAuthenticated ? '/connect' : loginPageLinkPath}>
+                    Connect
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="sidebar-button">
+                  <Link to={isAuthenticated ? '/codeFixer' : loginPageLinkPath}>
+                    CodeFixer
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="sidebar-button">
+                  <Link to={isAuthenticated ? `/inbox` : loginPageLinkPath}>
+                    Inbox
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="sidebar-button">
+                  <Link
+                    to={
+                      isAuthenticated ? `/profile/${userId}` : loginPageLinkPath
+                    }
+                  >
+                    Profile
+                  </Link>
+                </div>
+                <div className="sidebar-button">
+                  <Link
+                    to={isAuthenticated ? '/yourMeetings' : loginPageLinkPath}
+                  >
+                    Meetings
+                  </Link>
+                </div>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
